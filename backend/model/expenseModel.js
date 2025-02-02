7c3bd756c6aa63ea9be3e9184cb638369d0fb694
AadityaBajgain <155723003+AadityaBajgain@users.noreply.github.com>
2025-02-01 20:58:37 -0800

    Add user_id field to expense schema for expense tracking

--- backend/model/expenseModel.js
+++ backend/model/expenseModel.js
@@ -27,6 +27,10 @@ const expenseSchema = new Schema({
         type: String,
         required: true,
     },
+    user_id:{
+        type:String,
+        required:true
+    }
 });
 
 module.exports= mongoose.model('Expense', expenseSchema);
